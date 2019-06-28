fdd54bcdb699eab8e0c472263b71a979f0a7dd18
adisbladis <adisbladis@gmail.com>
2019-06-29 00:27:59 +0100

    Bump system channel to 19.03

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -euxo pipefail
-export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-18.09.tar.gz";
+export NIX_PATH="nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/nixos-19.03.tar.gz";
 export NIXOS_CONFIG=`readlink -f configuration.nix `
 
 nixos-rebuild --target-host root@nixcon.martinmyska.cz switch
