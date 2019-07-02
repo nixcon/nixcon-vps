f0437e79ea52d112a726af5a049f456290811f98
adisbladis <adisbladis@gmail.com>
2019-07-02 10:42:52 +0100

    Remove requests-toolbelt patch
    Unstable channel progressed and we dont need the patch

--- services/unstable.nix
+++ services/unstable.nix
@@ -10,13 +10,7 @@ in import (pkgs.stdenv.mkDerivation {
 
   inherit src;
 
-  patches = [
-    # Downgrade overmind
-    (pkgs.fetchpatch {
-      url = "https://github.com/NixOS/nixpkgs/commit/8df753c5983cdd4ca9b454570c7987feba8ebb2a.patch";
-      sha256 = "0rihi009a6scg60jqj3dssh4qszgyyzngm1zglci21bc7a1gmd19";
-    })
-  ];
+  patches = [ ];
 
   dontBuild = true;
 
